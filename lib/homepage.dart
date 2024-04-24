182b7a95f789d79560f76b41a57041bb7d13d900
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-24 15:03:28 +0000

    home updates

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -17,6 +17,20 @@ class _HomepageState extends State<Homepage> {
           flex: 3,
           child: Container(
             color: Colors.amber[100],
+            child: Center(
+              child: Stack(
+                children: [
+                  Container(
+                    alignment: const Alignment(0, 1),
+                    child: Container(
+                      color: Colors.black,
+                      height: 50.0,
+                      width: 50.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
         Expanded(
