94a966cd3f8737096b918f646ac60543a4cd29ae
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-24 15:51:20 +0000

    home updates

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:bubbleapp/button.dart';
 import 'package:bubbleapp/player.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -29,46 +32,57 @@ class _HomepageState extends State<Homepage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          flex: 3,
-          child: Container(
-            color: Colors.amber[100],
-            child: Center(
-              child: Stack(
+    return RawKeyboardListener(
+      focusNode: FocusNode(),
+      autofocus: true,
+      onKey: (event) {
+        if (event.isKeyPressed(LogicalKeyboardKey.arrowLeft)) {
+          moveLeft();
+        } else if (event.isKeyPressed(LogicalKeyboardKey.arrowRight)) {
+          moveRight();
+        }
+      },
+      child: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: Colors.amber[100],
+              child: Center(
+                child: Stack(
+                  children: [
+                    MyPlayer(
+                      playerX: playerX,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.grey[100],
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  MyPlayer(
-                    playerX: playerX,
+                  MyButton(
+                    icon: Icons.arrow_back,
+                    function: moveLeft,
+                  ),
+                  MyButton(
+                    icon: Icons.arrow_upward,
+                    function: fireMissle,
+                  ),
+                  MyButton(
+                    icon: Icons.arrow_forward,
+                    function: moveRight,
                   ),
                 ],
               ),
             ),
           ),
-        ),
-        Expanded(
-          child: Container(
-            color: Colors.grey[100],
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                MyButton(
-                  icon: Icons.arrow_back,
-                  function: moveLeft,
-                ),
-                MyButton(
-                  icon: Icons.arrow_upward,
-                  function: fireMissle,
-                ),
-                MyButton(
-                  icon: Icons.arrow_forward,
-                  function: moveRight,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
