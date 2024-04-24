f3aef68848068e533ecd9da1ed741ad1c8241894
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-24 14:24:17 +0000

    button updates

--- lib/button.dart
+++ lib/button.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton({super.key, this.icon});
+  const MyButton({super.key, this.icon, this.function});
   final icon;
+  final function;
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10.0),
-      child: Container(
-        color: Colors.amber,
-        height: 50.0,
-        width: 50.0,
-        child: Center(
-          child: Icon(
-            icon,
-            color: Colors.grey[100],
+    return GestureDetector(
+      onTap: function,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: Container(
+          color: Colors.amber,
+          height: 50.0,
+          width: 50.0,
+          child: Center(
+            child: Icon(
+              icon,
+              color: Colors.grey[100],
+            ),
           ),
         ),
       ),
