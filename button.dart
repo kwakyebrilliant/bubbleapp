b8529466fff17a70c40906db1d19d34408514692
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-24 14:03:14 +0000

    button updates

--- button.dart
+++ button.dart
@@ -6,6 +6,7 @@ class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.amber,
       height: 50.0,
       width: 50.0,
       child: Text('x'),
