9214ccf97c78e0881fc4d6ad4a91709b267a8cd4
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-24 13:44:28 +0000

    main updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bubbleapp/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: Homepage(),
     );
   }
 }
