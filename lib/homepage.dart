c8fcb5add4c06ea3752fc5c879ee2734385948f2
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-24 15:06:01 +0000

    home updates

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -20,12 +20,15 @@ class _HomepageState extends State<Homepage> {
             child: Center(
               child: Stack(
                 children: [
-                  Container(
-                    alignment: const Alignment(0, 1),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
                     child: Container(
-                      color: Colors.black,
-                      height: 50.0,
-                      width: 50.0,
+                      alignment: const Alignment(playerX, 1),
+                      child: Container(
+                        color: Colors.black,
+                        height: 50.0,
+                        width: 50.0,
+                      ),
                     ),
                   ),
                 ],
