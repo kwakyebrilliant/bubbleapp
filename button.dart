8605fd95a427397215bc7d2a0d28e94a117677c9
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-24 14:01:59 +0000

    button updates

---
+++ button.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.0,
+      width: 50.0,
+      child: Text('x'),
+    );
+  }
+}
