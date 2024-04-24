4e415f47daa2f7ae577f632e372979cf5271a734
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-24 14:15:58 +0000

    button updates

--- lib/button.dart
+++ lib/button.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton({super.key});
+  const MyButton({super.key, this.icon});
+  final icon;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber[200],
-      height: 50.0,
-      width: 50.0,
-      child: Text('x'),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10.0),
+      child: Container(
+        color: Colors.amber,
+        height: 50.0,
+        width: 50.0,
+        child: Center(
+          child: Icon(
+            icon,
+            color: Colors.grey[100],
+          ),
+        ),
+      ),
     );
   }
 }
