3b1bbc447c3d189b7fa68ec80828fe631c9fd0d5
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-04-29 08:21:37 +0000

    updates

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -106,7 +106,7 @@ class _HomepageState extends State<Homepage> {
     }
   }
 
-  //convert heights to cordinates
+  //convert heights to cordinate
   double heightToCordinate(double height) {
     double totalHeight = MediaQuery.of(context).size.height * 3 / 4;
     double missileY = 1 - 2 * height / totalHeight;
